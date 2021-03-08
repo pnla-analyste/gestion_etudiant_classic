603b36961af9afe6ee0cf38b7be2ac9e6f03c781
arnol <arnol@DESKTOP-E0CKFAK>
2021-03-08 01:04:26 +0100

    1- Modification du fichier application.properties Ajout de la propriete server.port au debut du fichier pour changer le port d'ecoute 2- Modification du controller   a-Prendre en consideration l'image d'un etudiant dans la consultation de la liste des etudiants 3- Modification de la vue etudiants.html  a- Ajout du champ universite  b- Modification de l'image pour l'afficher en forme arrondi 4- Modification du fichier pom.xml pour exploiter la classe IOUtils afin de prendre en compte l'image comme un tableau de bytes (la dependance commons-io est utilisee).

--- src/main/java/com/example/demo/web/EtudiantController.java
+++ src/main/java/com/example/demo/web/EtudiantController.java
@@ -1,18 +1,25 @@
 package com.example.demo.web;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
 
 import com.example.demo.dao.EtudiantRepository;
 import com.example.demo.entities.Etudiant;
@@ -24,7 +31,7 @@ public class EtudiantController {
 	@Autowired
 	private EtudiantRepository etudiantRepository;
 
-	public static String uploadDirectory = System.getProperty("user.home")+"/uploads";
+	public static String uploadDirectory = System.getProperty("user.home")+"/uploads/";
 	
 	public EtudiantController() {}
 	
@@ -32,7 +39,7 @@ public class EtudiantController {
 	public String Index(Model model,  @RequestParam(name = "p", defaultValue = "0") int p,  
 															   @RequestParam(name = "mc", defaultValue = "") String mc) {
 		
-		Page<Etudiant> list_etudiants = etudiantRepository.findByKey("%"+mc+"%", PageRequest.of(p, 4));
+		Page<Etudiant> list_etudiants = etudiantRepository.findAll(PageRequest.of(p, 4));
 		
 		int pagesCount = list_etudiants.getTotalPages();
 		int[] pages = new int[pagesCount];
@@ -43,6 +50,11 @@ public class EtudiantController {
 		model.addAttribute("pages", pages);
 		model.addAttribute("pageCourante", p);
 		model.addAttribute("motClef", mc);
+		
+		if (!mc.equalsIgnoreCase("")) {
+			list_etudiants = etudiantRepository.findByKey("%"+mc+"%", PageRequest.of(p, 4));
+		}
+		
 		model.addAttribute("etudiants",  list_etudiants); 
 		return "etudiants";
 	}
@@ -56,16 +68,35 @@ public class EtudiantController {
 	@RequestMapping(value="/addEtudiant", method=RequestMethod.POST)
 	public String create(Etudiant e,  @RequestParam(name = "image") MultipartFile file) throws Exception {
 	
-		//1- Ecriture physique (Mettre dans le dossier upload l'image)
-		 StringBuilder fileName = new StringBuilder();
-		 Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
-		 fileName.append(file.getOriginalFilename());
-		 Files.write(fileNameAndPath, file.getBytes());
+		//1- Ecriture logique (Inserer dans la table etudiant un enregistrement de type etudiant avec le nom de sa photo)
+		if (!(file.isEmpty())) { 
+			 e.setPhoto(file.getOriginalFilename());
+		}
+		
+		Etudiant et = etudiantRepository.save(e);
 		 
-		//2- Ecriture logique (Inserer dans la table etudiant un enregistrement de type etudiant avec le nom de sa photo) 
-		 e.setPhoto(fileName.toString());
-		 etudiantRepository.save(e);
+		if (!(file.isEmpty())) { 
+			
+			//2- Ecriture physique (Mettre dans le dossier upload l'image)
+			   //2.1- Nom du fichier (Nom du flux : contenant)
+			 		StringBuilder fileName = new StringBuilder();
+			 		String name = String.valueOf(et.getId());
+			 		fileName.append(name);
+			 		Path fileNameAndPath = Paths.get(uploadDirectory, fileName.toString());
+			  //2.2- Contenu du fichier (Flux physique : contenu)
+			 		Files.write(fileNameAndPath, file.getBytes());	
+		}
 		
 		 return "redirect:index";
+	}
+	
+	@RequestMapping(value="/getPhoto", 
+			produces=MediaType.IMAGE_JPEG_VALUE)
+	@ResponseBody
+	public byte[] getEtudiantPhoto(Long id) throws Exception {
+		String name = uploadDirectory+id;
+		File f = new File(name);
+		
+		return IOUtils.toByteArray(new FileInputStream(f));
 	}
 }
